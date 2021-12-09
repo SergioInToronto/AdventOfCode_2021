f92b459c7fd692cee789ba7e0e66254ef002d2c7
Sergio <sergio@opslevel.com>
2021-12-09 18:23:18 -0500

    part 2 (doesn't work)

--- day8.py
+++ day8.py
@@ -9,27 +9,21 @@ max_x = len(grid) - 1
 max_y = len(grid[0]) - 1
 
 
-# class coord():
-#     def __init__(self, x, y):
-#         self.x = x
-#         self.y = y
-
-#     def neightbours(self):
-#         result = []
-#         pass
+def neighbours(x, y):
+    result = []
+    if 0 <= x - 1:
+        result.append((x-1, y))
+    if x + 1 <= max_x:
+        result.append((x+1, y))
+    if 0 <= y - 1:
+        result.append((x, y-1))
+    if y + 1 <= max_y:
+        result.append((x, y+1))
+    return result
 
 
 def neighbour_values(x, y):
-    result = []
-    if 0 <= x - 1:
-        result.append(grid[x-1][y])
-    if x + 1 <= max_x:
-        result.append(grid[x+1][y])
-    if 0 <= y - 1:
-        result.append(grid[x][y-1])
-    if y + 1 <= max_y:
-        result.append(grid[x][y+1])
-    return result
+    return [grid[x][y] for (x, y) in neighbours(x, y)]
 
 
 def part1():
@@ -44,4 +38,43 @@ def part1():
     print(f"Sum of all risk levels: {total_risk_level}")
 
 
-part1()
+def get_low_points():
+    low_point_coords = []
+    for x, row in enumerate(grid):
+        for y, cell in enumerate(row):
+            if all(cell < nv for nv in neighbour_values(x, y)):
+                # print(f"Low point {cell} at grid[{x}][{y}]")
+                low_point_coords.append((x, y))
+    return low_point_coords
+
+
+def basin_size(low_point):
+    condition = lambda value: value < 9
+    seen = []
+    to_see = [low_point]
+    while to_see:
+        coords = to_see.pop(0)
+        seen.append(coords)
+        point_neighbours = neighbours(*coords)
+        basin_neighbours = [(x, y) for (x, y) in point_neighbours if grid[x][y] < 9]
+        new_neighbours = [n for n in basin_neighbours if n not in seen]
+        to_see.extend(new_neighbours)
+    print(f"Basin at {low_point} size: {len(seen)}")
+    return len(seen)
+
+
+def part2():
+    low_point_coords = get_low_points()
+    print(f"Computing sizes for {len(low_point_coords)} basins...")
+    basin_sizes = [basin_size(low_point) for low_point in low_point_coords]
+    basin_sizes = sorted(basin_sizes, reverse=True)
+    print(f"Top 3 basins sizes: {basin_sizes[0]}, {basin_sizes[1]}, {basin_sizes[2]}")
+    answer = basin_sizes[0] * basin_sizes[1] * basin_sizes[2]
+    print(f"Final answer: {answer}")
+    # 821702089 is too high
+    # Gets the wrong answer. I double-checked several basins & seems correct. Leaving this alone for now.
+
+
+# part1()
+part2()
+# basin_size((99, 13))
